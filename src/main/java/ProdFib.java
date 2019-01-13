c77d5cd3ee9c55a96790ec67b445b6fa9c1d55df
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-01-13 12:05:22 +0200

    Product of consecutive Fib numbers - refactoring

--- src/main/java/ProdFib.java
+++ src/main/java/ProdFib.java
@@ -1,28 +1,14 @@
 public class ProdFib { // must be public for codewars
 
     public static long[] productFib(long prod) {
-        int n = 0;
-        int n1 = 1;
+        long a = 0;
+        long b = 1;
 
-        long fib = getFib(n);
-        long fib1 = getFib(n1);
-        while (fib * fib1 < prod) {
-            fib = getFib(++n);
-            fib1 = getFib(++n1);
+        while (a * b < prod) {
+            long prev = b;
+            b = a + b;
+            a = prev;
         }
-        return new long[]{fib, fib1, fib * fib1 == prod ? 1 : 0};
-    }
-
-    private static long getFib(int n) {
-        if (n == 0)
-            return 0;
-
-        long[] fibs = {0, 1};
-        for (int i = 2; i <= n; i++) {
-            long prev = fibs[1];
-            fibs[1] = fibs[0] + fibs[1];
-            fibs[0] = prev;
-        }
-        return fibs[1];
+        return new long[]{a, b, a * b == prod ? 1 : 0};
     }
 }
