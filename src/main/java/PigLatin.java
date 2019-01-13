ed4d6842b1e1a89d96bb9710ae31886077436bbf
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-01-13 12:30:53 +0200

    Simple Pig Latin - refactored

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,12 +1,14 @@
+
+/**
+ * Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+ *
+ * Examples
+ * pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+ * pigIt('Hello world !');     // elloHay orldway !
+ *
+ * */
 public class PigLatin {
     public static String pigIt(String str) {
-        String result = "";
-        for(String word: str.split(" ")) {
-            if(word.length() == 1){
-                result += word + (Character.isLetter(word.charAt(0)) ? "ay " : " ");
-            } else
-                result += word.substring(1) + word.charAt(0) + "ay ";
-        }
-        return result.trim();
+        return str.replaceAll("(\\w)(\\w*)", "$2$1ay");
     }
 }
